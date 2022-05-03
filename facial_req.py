84c758b8890527ff7acbfd0dba01f41997709765
Ivan Lau <56351326+lwk1008@users.noreply.github.com>
2022-05-03 22:27:21 +0800

    face recognition for raspberry pi

---
+++ facial_req.py
@@ -0,0 +1,162 @@
+#! /usr/bin/python
+
+
+# import the necessary packages
+# from sense_hat import SenseHat
+from imutils.video import VideoStream
+from imutils.video import FPS
+import face_recognition
+import imutils
+import pickle
+import time
+import cv2
+
+from gpiozero import AngularServo
+servo = AngularServo(18, initial_angle=90,min_pulse_width=0.0006, max_pulse_width=0.0023)
+
+#Initialize 'currentname' to trigger only when a new person is identified.
+currentname = "unknown"
+#Determine faces from encodings.pickle file model created from train_model.py
+encodingsP = "encodings.pickle"
+
+# load the known faces and embeddings along with OpenCV's Haar
+# cascade for face detection
+print("[INFO] loading encodings + face detector...")
+data = pickle.loads(open(encodingsP, "rb").read())
+
+# initialize the video stream and allow the camera sensor to warm up
+# Set the ser to the followng
+# src = 0 : for the build in single web cam, could be your laptop webcam
+# src = 2 : I had to set it to 2 inorder to use the USB webcam attached to my laptop
+#vs = VideoStream(src=2,framerate=10).start()
+vs = VideoStream(usePiCamera=True).start()
+time.sleep(2.0)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+
+# start the FPS counter
+fps = FPS().start()
+
+G = (0,255,0)
+O = (0,0,0)
+R = (255,0,0)
+logo_ok=[
+O,O,O,O,O,O,O,O,
+O,G,G,O,O,G,O,G,
+G,O,O,G,O,G,G,O,
+G,O,O,G,O,G,O,O,
+G,O,O,G,O,G,O,O,
+G,O,O,G,O,G,G,O,
+O,G,G,O,O,G,O,G,
+O,O,O,O,O,O,O,O,
+]
+	
+logo_R=[
+O,G,G,G,G,G,O,O,
+O,G,O,O,O,G,O,O,
+O,G,O,O,G,O,O,O,
+O,G,G,G,O,O,O,O,
+O,G,G,O,O,O,O,O,
+O,G,O,G,O,O,O,O,
+O,G,O,O,G,O,O,O,
+O,G,O,O,O,G,O,O,
+]
+
+logo_cross=[
+R,O,O,O,O,O,O,R,
+O,R,O,O,O,O,R,O,
+O,O,R,O,O,R,O,O,
+O,O,O,R,R,O,O,O,
+O,O,O,R,R,O,O,O,
+O,O,R,O,O,R,O,O,
+O,R,O,O,O,O,R,O,
+R,O,O,O,O,O,R,R,
+R,O,O,O,O,O,R,R,
+]
+
+
+# loop over frames from the video file stream
+while True:
+	# grab the frame from the threaded video stream and resize it
+	# to 500px (to speedup processing)
+	frame = vs.read()
+	frame = imutils.resize(frame, width=500)
+	# Detect the fce boxes
+	boxes = face_recognition.face_locations(frame)
+	# compute the facial embeddings for each face bounding box
+	encodings = face_recognition.face_encodings(frame, boxes)
+	names = []
+
+	# loop over the facial embeddings
+	for encoding in encodings:
+		# attempt to match each face in the input image to our known
+		# encodings
+		matches = face_recognition.compare_faces(data["encodings"],
+			encoding)
+		name = "Unknown" #if face is not recognized, then print Unknown
+
+		# check to see if we have found a match
+		if True in matches:
+			# find the indexes of all matched faces then initialize a
+			# dictionary to count the total number of times each face
+			# was matched
+			matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+			counts = {}
+
+			# loop over the matched indexes and maintain a count for
+			# each recognized face face
+			for i in matchedIdxs:
+				name = data["names"][i]
+				counts[name] = counts.get(name, 0) + 1
+
+			# determine the recognized face with the largest number
+			# of votes (note: in the event of an unlikely tie Python
+			# will select first entry in the dictionary)
+			name = max(counts, key=counts.get)
+
+			#If someone in your dataset is identified, print their name on the screen
+			if currentname != name:
+				currentname = name
+				print(currentname)
+				# sense.set_pixels(logo_R)
+				
+				servo.angle=-90
+				time.sleep(5)
+				servo.angle=90
+			elif currentname == name:
+				print(currentname)
+				# sense.set_pixels(logo_R)
+				
+				servo.angle=-90
+				time.sleep(5)
+				servo.angle=90
+				
+		# update the list of names
+		names.append(name)
+	
+	# loop over the recognized faces
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+		# draw the predicted face name on the image - color is in BGR
+		cv2.rectangle(frame, (left, top), (right, bottom),
+			(0, 255, 225), 2)
+		y = top - 15 if top - 15 > 15 else top + 15
+		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			.8, (0, 255, 255), 2)
+
+	# display the image to our screen
+	cv2.imshow("Facial Recognition is Running", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# quit when 'q' key is pressed
+	if key == ord("q"):
+		break
+
+	# update the FPS counter
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
